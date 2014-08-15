2605cea3821330104e3e9de80d7e3b5cca3acff4
lycc001 <lyccemail@163.com>
2014-08-15 22:42:46 +0800

    add some string to the strings.xml

--- debula/gen/com/stuofuestc/debula/R.java
+++ debula/gen/com/stuofuestc/debula/R.java
@@ -53,6 +53,11 @@ public final class R {
         public static final int action_example=0x7f050006;
         public static final int action_settings=0x7f050007;
         public static final int app_name=0x7f050000;
+        /** Navigation Drawer  
+         */
+        public static final int cell_note=0x7f050008;
+        public static final int cell_settings=0x7f05000a;
+        public static final int cell_trash=0x7f050009;
         public static final int navigation_drawer_close=0x7f050005;
         public static final int navigation_drawer_open=0x7f050004;
         public static final int title_section1=0x7f050001;
